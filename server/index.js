fc8a2b113aec3567236c67e5f32d10b72f8fabf8
Gaston Capdevila <gaston.capdevila118@gmail.com>
2024-02-05 21:25:38 -0300

    recuperamos los msg, falta los users

---
+++ server/index.js
@@ -0,0 +1,82 @@
+import express from 'express';
+import logger from 'morgan';
+import dotenv from 'dotenv';
+
+import { createClient } from '@libsql/client'
+import { Server } from 'socket.io';
+import { createServer } from 'node:http';
+
+dotenv.config();
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+    connectionStateRecovery: {}
+});
+
+const db = createClient({
+    url: "libsql://equipped-forerunner-gaston118.turso.io",
+    authToken: process.env.LIBSQL_AUTH_TOKEN
+});
+
+await db.execute("CREATE TABLE IF NOT EXISTS messages (id INTEGER PRIMARY KEY AUTOINCREMENT, content TEXT)");
+
+
+
+io.on('connection', async (socket) => {
+    console.log('a user connected');
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+
+    socket.on('chat message', async (msg) => {
+        console.log('message: ' + msg);
+
+        let result
+        try{
+            result = await db.execute({
+                sql: "INSERT INTO messages (content) VALUES (:msg)",
+                args: { msg }
+            })
+        }catch(e){
+            console.log(e)
+            return
+        }
+
+        io.emit('chat message', msg, result.lastInsertRowid.toString());
+
+    });
+
+    console.log(socket.handshake.auth)
+
+    if(!socket.recovered){
+        try{
+            const result = await db.execute({
+                sql: "SELECT * FROM messages WHERE id > ?",
+                args: [socket.handshake.auth.serverOffset || 0]
+            })
+
+            result.rows.forEach(row => {
+                socket.emit('chat message', row.content, row.id.toString())
+            })
+            
+        }catch(e){
+            console.log(e)
+            return
+        }
+    }
+});
+
+app.use(logger('dev'));
+
+app.get('/', (req, res) => {
+  res.sendFile(process.cwd() + '/client/index.html')
+});
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
